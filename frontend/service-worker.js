b72613fb0fe3e5f95eee2ef202d452034560e2ca
danded778-gif <danded778@gmail.com>
2026-05-21 20:02:59 -0500

    version

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -6,7 +6,7 @@
 const isDev = false;
 const CACHE_NAME = isDev
     ? 'dev-' + Date.now() 
-    : 'soluvencon-v1.5.7'; 
+    : 'soluvencon-v1.5.8'; 
 
 // Archivos que se cachean al instalar
 const ARCHIVOS_ESTATICOS = [
