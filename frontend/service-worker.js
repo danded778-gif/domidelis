1d4bdadb71827ffb36c766bf51ed31feea90cc31
danded778-gif <danded778@gmail.com>
2026-05-23 16:18:18 -0500

    version

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -6,7 +6,7 @@
 const isDev = false;
 const CACHE_NAME = isDev
     ? 'dev-' + Date.now() 
-    : 'soluvencon-v1.5.7'; 
+    : 'soluvencon-v1.5.9'; 
 
 // ★ NUEVO: Baúl exclusivo para imágenes que NO se borrará con las actualizaciones de la app
 const IMAGES_CACHE_NAME = 'soluvencon-img-cache-v1'; 
