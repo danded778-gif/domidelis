6b287175c45ce82baa43dc8dd99641bbc122ca4f
danded778-gif <danded778@gmail.com>
2026-07-07 16:35:14 -0500

    sw

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -6,7 +6,7 @@
 const isDev = false;
 const CACHE_NAME = isDev
     ? 'dev-' + Date.now() 
-    : 'domidelis-v2.2'; 
+    : 'domidelis-v2.3'; 
 
 const IMAGES_CACHE_NAME = 'domidelis-img-cache-v1'; 
 
